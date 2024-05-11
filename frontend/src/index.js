47e644504f660c6cae30e19317fa3060f0940da5
mohd ayan raza <126085856+ayann07@users.noreply.github.com>
2024-05-11 23:39:08 +0530

    final commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,7 @@ import App from './App';
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import store from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist';
 
-
-let persistor = persistStore(store);
 
 export const BASE_URL="https://realtime-chat-app-eil8.onrender.com"
 // export const BASE_URL="http://localhost:8080"
@@ -17,9 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
         <Toaster />
-      </PersistGate>
     </Provider>
 )
